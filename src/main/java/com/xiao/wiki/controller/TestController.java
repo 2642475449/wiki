323126a3e6bbcd19abbe8b83931433cfe09b85ba
降蓝 <2642475449@qq.com>
2021-05-29 00:01:21 +0800

    开发Hello World接口

---
+++ src/main/java/com/xiao/wiki/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.xiao.wiki.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ：降蓝
+ * @description：测试
+ * @date ：2021/5/28 23:39
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
