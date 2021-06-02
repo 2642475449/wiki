03f9af6ac26fb10289edf06a9a7adc98d314991d
降蓝 <2642475449@qq.com>
2021-06-02 23:10:15 +0800

    添加了CopyUtil封装了BeanUtil

--- src/main/java/com/xiao/wiki/service/EbookService.java
+++ src/main/java/com/xiao/wiki/service/EbookService.java
@@ -4,11 +4,10 @@ import com.xiao.wiki.domain.Ebook;
 import com.xiao.wiki.domain.EbookExample;
 import com.xiao.wiki.mapper.EbookMapper;
 import com.xiao.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.xiao.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -28,14 +27,7 @@ public class EbookService {
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         criteria.andNameLike("%"+req.getName()+"%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
-
-        List<EbookResp> respList = new ArrayList<>();
-
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook,ebookResp);
-            respList.add(ebookResp);
-        }
-        return respList;
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+        return list;
     }
 }
